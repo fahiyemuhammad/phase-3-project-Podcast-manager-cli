c24725f9af29d6ad1c9a6678dc090828ad250789
Fahiye Muhammad <fahiye254@gmail.com>
2025-05-26 21:41:07 +0300

    calling the function in menu and running the cli from main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from database.setup import Base, engine
 from models.user import User
 from models.podcast import Podcast
+from cli.menu import main_menu
 
 Base.metadata.create_all(engine)
+
+main_menu()
